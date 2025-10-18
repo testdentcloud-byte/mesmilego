7053c3f87b4b5d2a3ee9ba2a6f872a270d73b720
testdentcloud-byte <testdentcloud@gmail.com>
2025-10-18 21:43:25 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,4 +1,4 @@
    
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz4J0kgkseIWm1VA2f_Z7i88eRmk0CU4gnQZC5bITaSNDfsxzy4TY6TD6wRCZSz3Kid/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyVc5QkF-x4cgW4ASIu0TLWGvyq6zWwzZLLN_aQ1uqIuxzBqzl23tKa5ow_Eqvqoywk/exec';
 const LIFF_ID = '2008293202-QjEJ8dj9'; // Booking
 const LIFF_ID_CONFIRM = '2008293202-VJQZWvzL'; 
