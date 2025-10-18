8650d67f3644a7c992f788c235e62fc12271d5f7
testdentcloud-byte <testdentcloud@gmail.com>
2025-10-18 21:44:24 +0700

    Update Config.js

--- Dashboard/Config.js
+++ Dashboard/Config.js
@@ -1,3 +1,4 @@
-const CONFIG_URL = 'https://script.google.com/macros/s/AKfycbx52mV2D_9JiKPO826l9cJjh6gE9wLKSd4BOSFipeHIsU-dwbgBLu6h06KfS398WbNo/exec';
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz4J0kgkseIWm1VA2f_Z7i88eRmk0CU4gnQZC5bITaSNDfsxzy4TY6TD6wRCZSz3Kid/exec';
+const CONFIG_URL = 'https://script.google.com/macros/s/AKfycbxPGdNi-LLsDDTYIKFOmfyFvle-NggkA53urDeJPf379xw-YEbxHgIrqX-hH8UxQcYj/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyVc5QkF-x4cgW4ASIu0TLWGvyq6zWwzZLLN_aQ1uqIuxzBqzl23tKa5ow_Eqvqoywk/exec';
+
 
