b4173b8f43e8a12985dcac5b908cdfc8c6b32049
IIIRN STUDIO <95156967+IIIRN@users.noreply.github.com>
2025-10-28 17:15:51 +0700

    Add files via upload

--- Dashboard/Config.js
+++ Dashboard/Config.js
@@ -1,4 +1,2 @@
 const CONFIG_URL = 'https://script.google.com/macros/s/AKfycbxPGdNi-LLsDDTYIKFOmfyFvle-NggkA53urDeJPf379xw-YEbxHgIrqX-hH8UxQcYj/exec';
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyVc5QkF-x4cgW4ASIu0TLWGvyq6zWwzZLLN_aQ1uqIuxzBqzl23tKa5ow_Eqvqoywk/exec';
-
-
