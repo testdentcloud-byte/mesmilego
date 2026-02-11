3fdef0ca2baecb0681b1d429999219a97e700a52
IIIRN (3RN) Studio <95156967+IIIRN@users.noreply.github.com>
2026-02-11 16:10:37 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,4 +1,5 @@
-   
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyVc5QkF-x4cgW4ASIu0TLWGvyq6zWwzZLLN_aQ1uqIuxzBqzl23tKa5ow_Eqvqoywk/exec';
+
 const LIFF_ID = '2008293202-QjEJ8dj9'; // Booking
+const LIFF_ID_HISTORY = '2008293202-5EPzcBAy';
 const LIFF_ID_CONFIRM = '2008293202-VJQZWvzL'; 
